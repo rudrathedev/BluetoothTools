51e81e861deff26ff370940a17a4db882a6d1b7c
Anshuman Mishra <82168547+rudrathedev@users.noreply.github.com>
2021-07-14 16:49:01 +0530

    Create BluetoothTools.java

---
+++ BluetoothTools.java
@@ -0,0 +1,77 @@
+package com.kab.BluetoothTools;
+
+import android.app.Activity;
+import android.content.Context;
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.ComponentCategory;
+import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
+import com.google.appinventor.components.runtime.ComponentContainer;
+import com.google.appinventor.components.runtime.EventDispatcher;
+import android.bluetooth.BluetoothAdapter;
+@DesignerComponent(
+        version = 1,
+        description = "",
+        category = ComponentCategory.EXTENSION,
+        nonVisible = true,
+        iconName = "")
+
+@SimpleObject(external = true)
+//Libraries
+@UsesLibraries(libraries = "android.permission.BLUETOOTH, android.permission.BLUETOOTH_ADMIN")
+//Permissions
+@UsesPermissions(permissionNames = "")
+
+public class BluetoothTools extends AndroidNonvisibleComponent {
+
+    //Activity and Context
+    private Context context;
+    private Activity activity;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    public BluetoothTools(ComponentContainer container){
+        super(container.$form());
+        this.activity = container.$context();
+        this.context = container.$context();
+    }
+
+    @SimpleFunction()
+    public void EnableBluetooth(){
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    mBluetoothAdapter.enable();
+    BluetoothEnabled();
+   
+    }
+    @SimpleFunction()
+    public void DisableBluetooth(){
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    mBluetoothAdapter.disable();
+    BluetoothDisabled();
+   }
+   @SimpleFunction()
+   public void CheckState(){
+    if (mBluetoothAdapter.isEnabled()) {
+    BluetoothEnabled();
+   }
+   else{
+       BluetoothDisabled();
+   }
+   if(mBluetoothAdapter == null){
+       BluetoothNotSupported();
+   } 
+
+   }
+   
+
+    @SimpleEvent()
+    public void BluetoothEnabled(){
+        EventDispatcher.dispatchEvent(this, "BluetoothEnabled");
+    }
+    @SimpleEvent()
+    public void BluetoothDisabled(){
+        EventDispatcher.dispatchEvent(this, "BluetoothDisabled");
+    }
+    @SimpleEvent()
+    public void BluetoothNotSupported(){
+        EventDispatcher.dispatchEvent(this, "BluetoothNotSupported");
+    }
+}
